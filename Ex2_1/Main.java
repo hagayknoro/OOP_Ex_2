c6db9edfbbac3ef5f52c8c624b35a23fd6c3ac50
hagayknoro <54978956+hagayknoro@users.noreply.github.com>
2023-01-10 12:15:36 +0200

    jon work

--- Ex2_1/Main.java
+++ Ex2_1/Main.java
@@ -1,10 +1,8 @@
 package Ex2_1;
 
-import Ex2_1.Ex2_1;
-
 import java.io.File;
 
-public class main {
+public class Main {
     public static void main(String[] args){
         int n =1000;
         long starTime, endTime;
